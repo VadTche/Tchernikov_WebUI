b6ef3e2805b8689e0e553b1eada8b165173933cb
VadTche <pismonos@gmail.com>
2021-04-08 18:56:26 +0300

    Lesson5

--- src/main/java/Lesson5Collectomania/Collectomania.java
+++ src/main/java/Lesson5Collectomania/Collectomania.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
@@ -26,8 +31,11 @@ public class Collectomania {
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
+
+    static Actions builder = new Actions(driver);
 
     public static void main(String[] args) throws InterruptedException {
         login();
@@ -46,11 +54,21 @@ public class Collectomania {
         driver.findElement(By.xpath(
                 ".//a[@href='#fancybox-login' and text()='Личный кабинет']")).click();
 
-        sleep(10000);
-        driver.findElement(By.xpath(".//div[@class='cnv-widget_popup-box']" +
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.presenceOfElementLocated
+                        (By.xpath(".//div[@class='cnv-widget_popup-box']" +
+                                "/div[@class='cnv-widget_popup-content']")));
+
+        builder.moveToElement(driver.findElement((By.xpath(".//div[@class='cnv-widget_popup-box']" +
                 "/div[@class='cnv-widget_popup-content']" +
-                "/div[@class='cnv-widget_popup-close']")).click();
-        sleep(5000);
+                "/div[@class='cnv-widget_popup-close']"))))
+                .click()
+                .build()
+                .perform();
+
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//form[@id='login_form']" +
+                        "/input[@class='input-email required']")));
 
         driver.findElement(By.xpath(".//form[@id='login_form']" +
                 "/input[@class='input-email required']")).sendKeys(USER_LOGIN);
